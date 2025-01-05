a6726c896868b6b1688a1808f14847132f30e436
jasonpham <phamthaitienthinh@gmail.com>
2025-01-04 21:46:29 -0500

    Basic Handle Data in Component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -11,5 +11,12 @@ export class TodoComponent {
   list: string[] = []; //empty list to hold the tasks
   task_var: string = ''; // task_var is the variable that will hold the task that the user inputs
 
+  addTask() {
+    if (this.task_var != '') {
+      this.list.push(this.task_var); //push the task into the list
+      this.task_var = ''; //clear the input field
+    }
+  }
 
+  
 }

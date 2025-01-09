253a752cd96ce92d938a74b664a21fabd2d20680
jasonpham <phamthaitienthinh@gmail.com>
2025-01-09 16:31:42 -0500

    Added markIncompleted function, rename functions.

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,19 +10,42 @@ import { CommonModule } from '@angular/common';
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
-  lists: string[] = []; //empty list to hold the tasks
+  tasks: { name: string; checked: boolean}[] = []; //empty list to hold the tasks
+
   task_var: string = ''; // task_var is the variable that will hold the task that the user inputs
 
+  completedTasks: { name: string; checked: boolean}[] = []; //empty list to hold the completed tasks
+ 
   addTask() {
     if (this.task_var != '') {
-      this.lists.push(this.task_var); //push the task into the list
+      this.tasks.push({ name: this.task_var, checked: false}); //push the task into the list
       this.task_var = ''; //clear the input field
     }
-    console.log(this.lists);
   }
 
   deleteTask(index: number) {
-    this.lists.splice(index, 1); //delete the task from the list
+    this.tasks.splice(index, 1); //delete the task from the list
+  }
+  
+  markCompleted(index: number) {
+    if (this.tasks[index].checked === true) {
+      this.tasks[index].checked = false;      
+    } else
+      this.tasks[index].checked = true;
+      const completedTask = this.tasks[index];
+      this.completedTasks.push(completedTask);
+      this.tasks.splice(index, 1);
+
+    console.log(this.completedTasks);
+  }
+
+  markIncompleted(index: number) {
+    this.completedTasks[index].checked = false;
+    const incompleteTasks = this.completedTasks[index];
+    this.tasks.push(incompleteTasks);
+    this.completedTasks.splice(index, 1);
+
+    console.log(this.tasks);
   }
   
 
